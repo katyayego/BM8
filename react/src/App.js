80eebe61774a9ad664a8e1940f3f5dfd20d71b71
Andrew Masek <masek@Andrews-MacBook-Pro.local>
2021-01-22 20:43:39 -0500

    web folder

---
+++ react/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import React from 'react';
+import { Button } from '@material-ui/core';
+
+function App() {
+  return (
+    <Button color="primary">Hello World</Button>
+  );
+}
+
+export default App;
