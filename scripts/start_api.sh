cce29c9075d439875f4945001683ac197a786282
Harrison McCarty <mccarth@purdue.edu>
2021-01-23 12:04:18 -0500

    forgot export

--- scripts/start_api.sh
+++ scripts/start_api.sh
@@ -2,4 +2,7 @@
 
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source $dir/../env/bin/activate
+export FLASK_APP=$dir/../wsgi.py
+export FLASK_DEBUG=1
+export DATABASE_URI=$dir/../mydb.db
 python3 -m flask run
