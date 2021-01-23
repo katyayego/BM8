ff1758b756803772f9382f3bbc75018ee1dc3494
Andrew Masek <masek@Andrews-MacBook-Pro.local>
2021-01-22 23:06:47 -0500

    setup roadmap page frontend

--- react/src/pages/Home/Home.js
+++ react/src/pages/Home/Home.js
@@ -1,8 +1,34 @@
 import React from 'react';
+import { Grid } from '@material-ui/core';
+import { Box } from '@material-ui/core';
+import { Card } from '@material-ui/core';
+import { CardHeader } from '@material-ui/core';
 
 const Home = () => {
   return (
-    <p>Home</p>
+    <Box m={2}>
+      <Grid container spacing={3} direction="column" alignItems="stretch">
+        <Grid item md="12" >
+          <Card>
+            <CardHeader title={'Purdue University MA 162'} />
+          </Card>
+        </Grid>
+      </Grid>
+      <Grid container spacing={3} justify="center" alignItems="stretch" direction="row">
+        <Grid item md="3" >
+          <Card>
+            <CardHeader title={'Topics'} />
+            <p>Load in topics belonging to this subject here</p>
+          </Card>
+        </Grid>
+        <Grid item md="8">
+          <Card>
+            <CardHeader title={'Roadmap'} />
+            <p>insert digraph here</p>
+          </Card>
+        </Grid>
+      </Grid>
+    </Box>
   );
 };
 
