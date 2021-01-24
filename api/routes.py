11514cfdf412a03f00fe5228d6df011f39a6760e
Harrison McCarty <mccarth@purdue>
2021-01-24 01:01:32 -0500

    routes

--- api/routes.py
+++ api/routes.py
@@ -130,9 +130,10 @@ def map_add_node():
 
         for e in edges:
             edge = {
-                'id'  : uuid.uuid1(),
-                'from': e,
-                'to'  : node['id']
+                'id'   : uuid.uuid1(),
+                'from' : e,
+                'to'   : node['id'],
+                'value': 1
             }
             map['edges'].append(edge)
         map = json.dumps(map)
@@ -176,9 +177,10 @@ def map_edit_node():
 
             for e in edges:
                 edge = {
-                    'id'  : uuid.uuid1(),
-                    'from': e,
-                    'to'  : map['nodes'][node_idx]['id']
+                    'id'   : uuid.uuid1(),
+                    'from' : e,
+                    'to'   : map['nodes'][node_idx]['id'],
+                    'value': 1
                 }
                 map['edges'].append(edge)
         
